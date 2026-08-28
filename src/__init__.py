15d6ba6c7bdd013754a89825fae08adef3de65e5
Pop Alexandru <alexandrupp55@gmail.com>
2026-08-28 12:18:14 +0300

    Add package metadata and exports in __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,13 @@
+"""
+LeNet-5 From Scratch Package
+"""
+from .model import LeNet_5, LeNetRBFSublayer, build_digit_bitmaps
 
+__all__ = [
+    "LeNet_5",
+    "LeNetRBFSublayer",
+    "build_digit_bitmaps",
+]
+
+__version__ = "1.0.0"
+__author__ = "Pop Alexandru"
